e4f6d215da30fdf894bf3a24cda710f46de73f11
Masuma <masumaakterakhi90@gmail.com>
2025-07-23 00:15:46 +0600

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
+dotenv.config();
+const app = express();
+const port = process.env.PORT || 5250;
+// Middleware
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000',
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+
+
+// Test route
+app.get('/', (req, res) => {
+  res.send('API is Running...');
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
